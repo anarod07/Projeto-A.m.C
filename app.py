16f65eab599533c145516675b380b20765a8b0fd
anarod07 <136479023+anarod07@users.noreply.github.com>
2024-12-20 01:35:32 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app= Flask(__name__)
+
+#rota para página inicial
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+#rota para página de artistas
+@app.route('/artistas')
+def artistas():
+    return render_template("artistas.html")
+
+#rota para página de discos
+@app.route('/discos')
+def discos():
+    return render_template("discos.html")
+
+#rota para página de login
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+#rota para página de quem somos
+@app.route('/sobre-nós')
+def sobre():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
